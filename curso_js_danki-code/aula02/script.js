8630a6842f8df75aca36d1a397112e29622b3a94
aleanrocha <alean.oficiall@gmail.com>
2023-01-01 16:12:43 -0300

    variaveis

---
+++ curso_js_danki-code/aula02/script.js
@@ -0,0 +1,26 @@
+alert('Funcionando...')
+
+// var - variável
+// let - variável de escopo
+// const - constante
+
+var nome = 'Zezinho'
+console.log(nome)
+nome = 'javascript'
+console.log(nome)
+
+{
+    let idade = 25
+    // console.log(idade)
+}
+// console.log(idade)
+
+// uma constante não pode ter seu valor alterado
+const nota = 7
+//nota = 5
+console.log(nota)
+
+let nome2 = 'beto'
+let idade = 12
+let solteiro = true
+console.log("Seu nome é " + nome2 + " você tem " + idade + " de idade")
