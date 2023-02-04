e346934862fcece1ad0915b78f1d436449d80edf
aleanrocha <alean.oficiall@gmail.com>
2023-02-04 12:23:29 -0300

    exercicio 01

---
+++ curso_js_danki-code/aula09/script.js
@@ -0,0 +1,13 @@
+// primeiro exercício
+
+// o prompt abre um campo para digitar algo
+
+let nome = prompt('Qual o seu nome?')
+let idade = prompt('Qual a sua idade?')
+let cidade = prompt('Qual a sua cidade?')
+
+alert('Seu nome é ' + nome + ' você tem ' + idade + ' anos e você mora em ' + cidade)
+
+console.log(nome)
+console.log(idade)
+console.log(cidade)
