332f5997005df297a1321221c898bfeec1ceb978
aleanrocha <alean.oficiall@gmail.com>
2023-02-04 12:15:44 -0300

    funcoes part2

---
+++ curso_js_danki-code/aula06/script.js
@@ -0,0 +1,18 @@
+// funções - Basicamente as funções são trechos
+// de codigos que podemos chamar a qualquer momento
+
+// Outras maneiras de declarar funções
+
+
+/*
+const f = function () {
+    alert('Nova função')
+}
+()
+*/
+
+// função que é chamada automaticamente
+(function () {
+    console.log("Função chamada automaticamente.")
+    alert("Função chamada automaticamente.")
+})()
