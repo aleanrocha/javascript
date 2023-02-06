3f71cdb47e0d3556613cd88b74e6ddd778efb842
aleanrocha <alean.oficiall@gmail.com>
2023-02-06 11:02:42 -0300

    datas p1

---
+++ curso_js_danki-code/aula16/script.js
@@ -0,0 +1,15 @@
+// Trabalhando com datas em JavaScript
+
+let data = new Date()
+
+// pega o dia atual
+
+alert(data.getDate())
+
+// pega o mes atual 
+alert(data.getMonth()+1)
+
+// pega o ano atual
+alert(data.getFullYear())
+
+
