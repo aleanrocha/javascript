863ba2d316414e1cf5dad5c86fa3d1767c389aa3
aleanrocha <alean.oficiall@gmail.com>
2023-02-06 12:12:16 -0300

    manipulando css atraves das classes

--- curso_js_danki-code/aula19/script.js
+++ curso_js_danki-code/aula19/script.js
@@ -1,7 +1,17 @@
-// pegando elementos pela id - DOM
+// pegando elementos pela classes - DOM
 
-let box = document.getElementById('box')
+let box = document.getElementsByClassName('box')
 
-box.style.width = '200px'
-box.style.height = '200px'
-box.style.backgroundColor = 'darkblue'
+/*
+box[0].style.width = '200px'
+box[0].style.height = '200px'
+box[0].style.backgroundColor = 'lightgreen'
+*/
+
+for(let i = 0 ; i < box.length ; i ++) {
+    box[i].style.width = '200px'
+    box[i].style.height = '200px'
+    box[i].style.backgroundColor = 'lightgreen'
+    box[i].style.display = 'inline-block'
+    box[i].style.marginBottom = '10px'
+}
