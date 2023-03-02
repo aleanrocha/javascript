e240d0c260249cc82105547722ee3c399c9b02a1
aleanrocha <alean.oficiall@gmail.com>
2023-03-02 10:49:02 -0300

    tipo de dado number

---
+++ curso_js_hora-de-codar/a5_number/js/script.js
@@ -0,0 +1,32 @@
+/*
+  No javascript existe vários tipos de dados, os mais comuns são:
+  NUMBER
+  STRING
+  BOOLEAN
+  NULL
+  UNDEFINED
+*/
+
+// Utilizando o NUMBER
+
+let numero = 5
+console.log(numero)
+console.log(typeof numero)
+
+let float = 10.5
+console.log(float)
+console.log(typeof float)
+
+let textoComNumero = "525"
+console.log(textoComNumero)
+console.log(typeof textoComNumero)
+
+console.log(NaN)
+console.log(typeof NaN)
+
+console.log(+Infinity)
+console.log(typeof +Infinity)
+
+console.log(-Infinity)
+console.log(typeof -Infinity)
+
