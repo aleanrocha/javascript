ed1df74b429df51fef9ad577b3bafd1aa8a27e80
aleanrocha <alean.oficiall@gmail.com>
2023-03-02 10:47:13 -0300

    particularidades em js

---
+++ curso_js_hora-de-codar/a3_particularidades-js/js/script.js
@@ -0,0 +1,28 @@
+console.log("Primeiro")
+
+console.log("Último")
+
+console.log("Mais um console.log")
+
+// declara uma váriavel
+var x = 10
+
+console.log(x)
+
+var x = "teste"
+
+console.log(x)
+
+// Este é um comentário
+
+/*
+
+Este
+é
+um
+comentário
+de
+múçtiplas
+linhas
+
+*/
