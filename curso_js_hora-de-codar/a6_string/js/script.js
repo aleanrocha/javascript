7f6d17449bf180053b0f83550a504efbf8c309f6
aleanrocha <alean.oficiall@gmail.com>
2023-03-02 10:49:43 -0300

    tipo de dado - string

---
+++ curso_js_hora-de-codar/a6_string/js/script.js
@@ -0,0 +1,28 @@
+/*
+  No javascript existe vários tipos de dados, os mais comuns são:
+  NUMBER
+  STRING
+  BOOLEAN
+  NULL
+  UNDEFINED
+*/
+
+// Utilizando STRING
+
+/*
+  As Strings são um tido de dado para textos
+  as strings podem ser escritas com aspas simples e duplas
+*/
+
+let nome = "Zezinho"
+console.log(nome)
+console.log(typeof nome)
+
+let sobrenome = 'Silva'
+console.log(sobrenome)
+console.log(typeof sobrenome)
+
+let nomeSobrenome = "Menu nome é " + nome + " meu sobrenome é " + sobrenome
+
+console.log(nomeSobrenome)
+console.log(typeof nomeSobrenome)
