fe6bc18b0a732bab9509346d7b1da84b114556c0
aleanrocha <alean.oficiall@gmail.com>
2023-03-02 10:48:01 -0300

    variaveis

---
+++ curso_js_hora-de-codar/a4_variaveis/js/script.js
@@ -0,0 +1,35 @@
+// Uma variável e um espaço na memória  do computador
+
+var num = 100
+
+console.log(num)
+
+num = "Mudei o tipo de dado da variável"
+
+console.log(num)
+
+var nome = "JavaScript"
+console.log(nome)
+
+var $n1 = 50
+console.log($n1)
+
+var _n2 = 20
+console.log(_n2)
+
+
+// outro tipo de vaiável
+
+// meuNome => CamelCase
+
+let meuNome = "Zezinho"
+
+console.log(meuNome)
+
+let sobrenome
+
+console.log(sobrenome)
+
+sobrenome = "Silva"
+
+console.log(sobrenome)
