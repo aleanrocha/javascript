8918f793375dac314fa6c192148be5f9da354231
aleanrocha <alean.oficiall@gmail.com>
2023-03-02 10:50:23 -0300

    tipo de dado - boolean

---
+++ curso_js_hora-de-codar/a7_boolean/js/script.js
@@ -0,0 +1,23 @@
+/*
+  No javascript existe vários tipos de dados, os mais comuns são:
+  NUMBER
+  STRING
+  BOOLEAN
+  NULL
+  UNDEFINED
+*/
+
+// Utilizando BOOLEAN
+
+/*
+  O Boolean representa um valor 
+  verdadeiro ou falso (true/false)
+*/
+
+let verdadeiro = true
+console.log(verdadeiro)
+console.log(typeof verdadeiro)
+
+let falso = false
+console.log(falso)
+console.log(typeof falso)
