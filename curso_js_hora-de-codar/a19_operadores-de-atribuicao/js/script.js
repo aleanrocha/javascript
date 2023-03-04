578996a44acb018221d5337ff1cbd638a58ec17a
aleanrocha <alean.oficiall@gmail.com>
2023-03-04 10:26:48 -0300

    operadores de atribuicao

---
+++ curso_js_hora-de-codar/a19_operadores-de-atribuicao/js/script.js
@@ -0,0 +1,41 @@
+/*
+  Temos algumas maneiras de atribuir um valor a uma
+  variável as mais utilizadas são:
+  =+
+  -=
+  *=
+  /=
+  Basicamente é uma forma resumida da operação (X = X + Y)
+  em loops também é comum utilizar os operadores (++) ou (--)
+*/
+
+let x = 1
+let y = 2
+
+// Adição 
+
+console.log(x = x + y)
+console.log(x+=y)
+console.log("----------")
+
+// Subtração
+
+console.log(x = x - 1)
+console.log(x-=y)
+console.log("----------")
+
+
+// Multiplicação
+
+console.log(x = x * y)
+console.log(x*=y)
+console.log("----------")
+
+// Divisão
+
+console.log(x = x / y)
+console.log(x/=y)
+
+// loops 
+// console.log(x++)
+// console.log(x--)
