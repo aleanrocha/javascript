abbf9d9bf6ba25f9a5c4d2dfaf9b97b63a25de55
aleanrocha <alean.oficiall@gmail.com>
2023-03-04 08:02:47 -0300

    comparacao de valor e tipo

---
+++ curso_js_hora-de-codar/a14_comparacao-de-tipo-e-valor/js/script.js
@@ -0,0 +1,16 @@
+let numero = '10'
+
+// verifica o valor
+if (numero == 10) {
+  console.log('O número é 10')
+}
+
+// verifica o valor é o tipo
+
+if (numero === 10) {
+  console.log('O número é 10 e do tipo number')
+}
+
+if (numero !== 10) {
+  console.log("O número 10 não é do tipo number")
+}
