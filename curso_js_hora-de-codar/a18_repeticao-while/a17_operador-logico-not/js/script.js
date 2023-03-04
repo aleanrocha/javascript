413cb830e8b2324065df4d81281132016b0fa1d4
aleanrocha <alean.oficiall@gmail.com>
2023-03-04 09:51:56 -0300

    repeticao while

---
+++ curso_js_hora-de-codar/a18_repeticao-while/a17_operador-logico-not/js/script.js
@@ -0,0 +1,31 @@
+// EStruturas de repetição
+
+// While
+
+let n = 0
+
+while (n < 5) {
+  console.log("Repetição " + n)
+  n++
+}
+
+let arr = ["Linguagem", "Programação", "JavaScript", "EcmaScript"]
+
+let i = 0
+
+console.log("---------------------")
+
+while (i < arr.length) {
+  console.log(arr[i])
+  i++
+}
+
+console.log("---------------------")
+
+let nome = "javascript"
+let tam = 0
+
+while (tam < nome.length) {
+  console.log(nome[tam])
+  tam++
+}
