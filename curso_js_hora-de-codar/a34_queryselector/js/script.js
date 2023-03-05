e494bf0b360ac9cca90146f3b0397cb882568eab
aleanrocha <alean.oficiall@gmail.com>
2023-03-05 11:43:46 -0300

    queryselectoe e queryselectorall

---
+++ curso_js_hora-de-codar/a34_queryselector/js/script.js
@@ -0,0 +1,17 @@
+let itemClasse = document.getElementsByClassName("item")
+console.log(itemClasse)
+
+// querySelectorAll
+
+let itemQuery1 = document.querySelectorAll("#lista1 li")
+console.log(itemQuery1)
+
+let itemQuery2 = document.querySelectorAll("#lista2 li") 
+console.log(itemQuery2)
+
+// querySelector
+
+let p = document.querySelector("p")
+let p_span = document.querySelector("p span")
+console.log(p)
+console.log(p_span)
