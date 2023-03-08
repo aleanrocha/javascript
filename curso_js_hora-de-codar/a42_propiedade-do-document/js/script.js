159b7a639afdd0efb3ed248e60d29e4bc0109d25
aleanrocha <alean.oficiall@gmail.com>
2023-03-08 10:23:29 -0300

    propiedade document

---
+++ curso_js_hora-de-codar/a42_propiedade-do-document/js/script.js
@@ -0,0 +1,15 @@
+/*
+  O objeto document não tem somente métodos
+  Podemos retirar várias informações importantes
+  das suas propiedades.
+*/
+
+console.log(document.head)
+
+console.log(document.body)
+
+console.log(document.links)
+
+document.links[0].textContent = "Tesla"
+
+console.log(document.URL)
