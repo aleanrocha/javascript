b552f2b855e8fa7044951f18ddff4e4f12c78b89
aleanrocha <alean.oficiall@gmail.com>
2023-03-09 08:29:20 -0300

    evento onload

---
+++ curso_js_hora-de-codar/a50_evento-onload/js/script.js
@@ -0,0 +1,28 @@
+/*
+
+  Por meio do JavaScript podemos mapear algumas 
+  acões dos usuários, que chamamos de eventos,
+  como por exemplo o movimento do mouse, click,
+  mouse entrando ou saindo de um elemento, carrehgamento
+  da página e etc. Com isso podemos então criar comportamentos 
+  interessantes como a animação de menu abrindo e fechando.
+
+*/
+
+// onload => é ativado depois de carregar a página
+
+window.onload = () => {
+  console.log("Página carregada!")
+
+  let h1 = document.querySelector("h1")
+
+  console.log(h1)
+
+}
+
+console.log("carregou o js")
+
+let h1 = document.querySelector("h1")
+
+console.log(h1)
+
